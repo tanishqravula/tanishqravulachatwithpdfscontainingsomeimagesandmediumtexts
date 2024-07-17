dd8853a86ae403e4ddc4164054fa254781140e02
tanishqravula <121391761+tanishqravula@users.noreply.github.com>
2024-07-17 22:49:18 +0530

    Update chatapp.py

--- chatapp.py
+++ chatapp.py
@@ -37,7 +37,7 @@ def get_pdf_text(pdf_docs):
     return text
 
 def get_text_chunks(text):
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=5000, chunk_overlap=500)
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=10000, chunk_overlap=1000)
     chunks = text_splitter.split_text(text)
     return chunks
 
